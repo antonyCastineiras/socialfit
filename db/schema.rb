e11bd3d26823f2dfd4653a6cb41101fd71ce15fd
antonyCastineiras <antonycastineiras80@gmail.com>
2017-03-07 13:31:56 +0000

    updated page layout slightly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307115149) do
+ActiveRecord::Schema.define(version: 20170307130134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
