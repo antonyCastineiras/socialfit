eb92be0916efec5f7ec8ee2e45357addacc69858
antonyCastineiras <antonycastineiras80@gmail.com>
2017-03-08 01:12:22 +0000

    displays the attending users on event/show

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :invites
 
   def organizer
   	user
+  end
+
+  def attending
+  	invites.where("accepted = true").collect {|i| i.user }
   end
 end
