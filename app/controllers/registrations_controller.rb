484946dd5c5dfd95d47008a71d52e347640b44e5
antonyCastineiras <antonycastineiras80@gmail.com>
2017-03-11 16:06:37 +0000

    added username to uers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
 	def after_sign_up_path_for(resource)
 		user_home_path
 	end
+
+	def sign_up_params
+		params.require(:user).permit(:email,:password,:password_confirmation,:username)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
