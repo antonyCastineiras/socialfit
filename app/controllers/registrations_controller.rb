c439b2e7858e06ede438e654639ec5feefc7444f
antonyCastineiras <antonycastineiras80@gmail.com>
2017-03-15 21:44:48 +0000

    users can update their details

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
 	protected
 
+	def update_resource(resource, params)
+		resource.update_without_password(params)
+	end
+
+	def account_update_params
+		params.require(:user).permit(:username)
+	end 
+
+	def after_update_path_for(resource)
+		user_home_path(page: 'account_settings', page_update: true)
+	end
+
 	def after_sign_up_path_for(resource)
 		user_home_path
 	end
