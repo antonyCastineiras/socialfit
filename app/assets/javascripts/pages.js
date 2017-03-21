b8dbf5c8ce269b141c93028ddc9b8180c8a707f4
antonyCastineiras <antonycastineiras80@gmail.com>
2017-03-21 17:37:54 +0000

    display the sign in/up forms on link click

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,27 @@
+$(document).on("turbolinks:load", function() {
+	hideSignInForm();
+	displayFormBehaviour();
+});
+
+function hideSignInForm() {
+	$('#home-page-forms-container #sign-in-form').hide();
+}
+
+function displayFormBehaviour() {
+	$('.display-form-link').on('click', function(event) {
+		event.preventDefault();
+		// only switch forms if parent element does not have the class active 
+		// parent must be targeted as that is the elemtn with active not the clicked link
+		if (!$(event.target).parent().hasClass('active')) {
+			toggleClass(this);
+			currentlyHidden = $('#home-page-forms-container form:hidden');
+			currentlyVisible = $('#home-page-forms-container form:visible');
+			currentlyVisible.slideToggle();
+			currentlyHidden.slideToggle();
+		}
+	});
+}
+
 function toggleClass(elem) {
 	$('.active').toggleClass('active');
 	$(elem).toggleClass('active');
